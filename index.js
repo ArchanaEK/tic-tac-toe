2f41a9c8596622beaed4f85cdc32b8c7abc468c0
Archana <archana.patel@eronkan.com>
2022-08-31 16:08:38 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let gameboard = {
+    button1:"button1",
+    button2:"button2",
+    button3:"button3",
+    button4:"button4",
+    button5:"button5",
+    button6:"button6",
+    button7:"button7",
+    button8:"button8",
+    button9:"button9"
+    
+}; ///gameboard Object to store the gameboard variables
+let players = {
+    playerX: 'X',
+    player0: '0'
+
+};
+
+let controlFlow = {} // Object to control the flow of control
+
+
