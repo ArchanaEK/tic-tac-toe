d9c092c88a17fcb4c60142f064200c50e1e58758
Archana <archana.patel@eronkan.com>
2022-09-01 11:58:30 +0530

    Completed project

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
-let gameboard = {
-    button1:"button1",
-    button2:"button2",
-    button3:"button3",
-    button4:"button4",
-    button5:"button5",
-    button6:"button6",
-    button7:"button7",
-    button8:"button8",
-    button9:"button9"
-    
-}; ///gameboard Object to store the gameboard variables
-let players = {
-    playerX: 'X',
-    player0: '0'
+function resetGame() {
+    let popup = document.getElementById("popup");
 
-};
+    console.log("open popup")
+    popup.classList.remove("open-popup");
+    document.getElementById('button1').value = '';
+    document.getElementById("button2").value = '';
+    document.getElementById("button3").value = '';
+    document.getElementById("button4").value = '';
+    document.getElementById("button5").value = '';
+    document.getElementById("button6").value = '';
+    document.getElementById("button7").value = '';
+    document.getElementById("button8").value = '';
+    document.getElementById("button9").value = '';
+}
 
-let controlFlow = {} // Object to control the flow of control
+function playAndGetResult() {
 
+    let b1 = document.getElementById('button1').value;
+    let b2 = document.getElementById('button2').value;
+    let b3 = document.getElementById('button3').value;
+    let b4 = document.getElementById('button4').value;
+    let b5 = document.getElementById('button5').value;
+    let b6 = document.getElementById('button6').value;
+    let b7 = document.getElementById('button7').value;
+    let b8 = document.getElementById('button8').value;
+    let b9 = document.getElementById('button9').value;
 
+    console.log({
+        b1,
+        b2,
+        b3
+    })
+    if (b1 == 'X' && b2 == 'X' && b3 == 'X' || b4 == 'X' && b5 == 'X' && b6 == 'X' || b7 == 'X' && b8 == 'X' && b9 == 'X' ||
+        b1 == 'X' && b4 == 'X' && b7 == 'X' || b2 == 'X' && b5 == 'X' && b8 == 'X' || b3 == 'X' && b6 == 'X' && b9 == 'X' ||
+        b1 == 'X' && b5 == 'X' && b9 == 'X' || b3 == 'X' && b5 == 'X' && b7 == 'X') {
+        document.getElementById('player').innerText = 'X'
+
+    } else if(b1 == '0' && b2 == '0' && b3 == '0' || b4 == '0' && b5 == '0' && b6 == '0' || b7 == '0' && b8 == '0' && b9 == '0' ||
+    b1 == '0' && b4 == '0' && b7 == '0' || b2 == '0' && b5 == '0' && b8 == '0' || b3 == '0' && b6 == '0' && b9 == '0' ||
+    b1 == '0' && b5 == '0' && b9 == '0' || b3 == '0' && b5 == '0' && b7 == '0'){
+        document.getElementById('player').innerText = '0'
+
+    }
+    else {
+        document.getElementById('player').innerText = 'Please enter the valid scenario to'
+    }
+
+    let popup = document.getElementById("popup");
+    console.log("popup opens")
+
+    console.log(popup)
+    popup.classList.add("open-popup")
+}
